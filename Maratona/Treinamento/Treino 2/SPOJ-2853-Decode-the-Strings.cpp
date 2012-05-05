c6e36bafedfc40b7c678367af36acb857b33c5de
Thiago Dourado de Andrade <thiago@thiago-desktop.(none)>
2012-05-05 01:30:02 -0400

    Treino 2 Maratona: Decode the Strings

---
+++ Maratona/Treinamento/Treino 2/SPOJ-2853-Decode-the-Strings.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int letras;
+	int repeticoes;
+	
+	cin >> letras >> repeticoes;
+	
+	while (letras != 0 && repeticoes != 0) {
+		int ordem[letras];
+		string linha;
+		
+		for (int i = 0; i < letras; i++) {
+			int temp;
+			
+			cin >> temp;
+			
+			ordem[temp - 1] = i;
+		}
+		
+		getline(cin, linha);
+		getline(cin, linha);
+		
+		string saida(linha);
+		//saida[linha.size()] = '\0';
+		
+		for (int i = 0; i < letras; i++) {
+			if (ordem[i] != i) {
+				vector<int> mutacao;
+				
+				while (ordem[i] != i) {
+					mutacao.push_back(ordem[i]);
+					
+					swap(ordem[i], ordem[ordem[i]]);
+				}
+				
+				mutacao.push_back(i);
+				
+				int periodo = repeticoes % mutacao.size();
+				
+				for (int i = 0; i < mutacao.size(); i++) {
+					saida[mutacao[i]] = linha[mutacao[(i + periodo) % mutacao.size()]];
+				}
+			}
+		}
+		
+		//string palavra(saida);
+		
+		//cout << linha << endl;
+		
+		cout << saida << endl;
+		
+		cin >> letras >> repeticoes;
+	}
+	
+	return 0;
+}
