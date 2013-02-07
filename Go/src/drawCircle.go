f8e1f30075536c2cedc97eacdb3d6871911cf1a5
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2013-02-07 19:12:00 -0400

    Funcionando mas feio

--- Go/src/drawCircle.go
+++ Go/src/drawCircle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"image"
 	"image/draw"
 	"image/color"
@@ -14,7 +15,8 @@ var (
 )
 
 const (
-	lado = 200
+	lado = 100
+	meio float64 = 0.5
 )
 
 func main() {
@@ -26,14 +28,16 @@ func main() {
 	
 	defer file.Close()
 	
-	imagem := image.NewRGBA(image.Rect(0, 0, lado, lado))
+	imagem := image.NewRGBA(image.Rect(-lado, -lado, lado, lado))
 	
 	draw.Draw(imagem, imagem.Bounds(), image.NewUniform(color.RGBA{40, 125, 80, 255}), image.ZP, draw.Src)
 	
-	for y := 0; y < lado; y++ {
-		for x := y; x < lado; x++ {
-			imagem.Set(x, y, cor)
-		}
+	for raio := 25; raio > 0; raio-- {
+		Circle(imagem, cor, float64(raio))
+		cor.R += 6
+		cor.G += 6
+		cor.B += 6
+		//fmt.Println(cor)
 	}
 	
 	err = png.Encode(file, imagem)
@@ -41,4 +45,38 @@ func main() {
 	if (err != nil) {
 		fmt.Println(err)
 	}
+}
+
+func Circle(imagem draw.Image, cor color.Color, raio float64) {
+	metade := raio * math.Cos(math.Pi / 4)
+	
+	for x := float64(0.5); x < metade; x++ {
+		y := math.Sin(math.Acos(x / raio)) * raio
+		
+		xInt := int(math.Floor(x + meio))
+		yInt := int(math.Floor(y + meio))
+		
+		FillArc(xInt - 1, yInt - 1, imagem, cor)
+		FillArc(xInt - 1, -yInt, imagem, cor)
+		FillArc(-xInt, yInt - 1, imagem, cor)
+		FillArc(-xInt, -yInt, imagem, cor)
+	}
+}
+
+func FillArc(horizontal, vertical int, imagem draw.Image, cor color.Color) {
+	var passo int
+	
+	if (vertical > 0) {
+		passo = 1
+	} else {
+		passo = -1
+	}
+	
+	for indice := 0; indice != vertical; indice += passo {
+		imagem.Set(horizontal, indice, cor)
+		imagem.Set(indice, horizontal, cor)
+	}
+	
+	imagem.Set(horizontal, vertical, cor)
+	imagem.Set(vertical, horizontal, cor)
 }
