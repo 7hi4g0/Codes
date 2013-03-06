d476516af5d8595418c642ec3b91592619ba95b4
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2013-03-06 19:49:10 -0400

    Programa simples de Prime Anagram

--- Go/src/perceptronTest.go
+++ Go/src/perceptronTest.go
@@ -9,19 +9,25 @@ func main() {
 	p := perceptron.New(4, 0.1, 0.5)
 	
 	base := []perceptron.Entrada{{0, 0, 0, 0, 0},
-								{0, 1, 0, 0, 0},
+								{0, 1, 0, 1, 1},
 								{1, 0, 1, 0, 0},
 								{1, 1, 0, 0, 0},
-								{0, 1, 0, 0, 0},
-								{1, 0, 1, 0, 0},
-								{1, 1, 0, 0, 0}}
+								{1, 1, 0, 1, 1},
+								{0, 1, 1, 1, 1},
+								{1, 1, 1, 1, 0},
+								{0, 0, 1, 1, 1}}
 	
 	fmt.Println(p)
-	p.Learn(base)
+	err := p.Learn(base)
 	
-	fmt.Println(p.Estimate(perceptron.Entrada{0, 0}))
-	fmt.Println(p.Estimate(perceptron.Entrada{0, 1}))
-	fmt.Println(p.Estimate(perceptron.Entrada{1, 0}))
-	fmt.Println(p.Estimate(perceptron.Entrada{1, 1}))
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	fmt.Println(p.Estimate(perceptron.Entrada{0, 0, 1, 0}))
+	fmt.Println(p.Estimate(perceptron.Entrada{1, 0, 1, 1}))
+	fmt.Println(p.Estimate(perceptron.Entrada{1, 0, 0, 0}))
+	fmt.Println(p.Estimate(perceptron.Entrada{1, 1, 1, 0}))
+	fmt.Println(p.Estimate(perceptron.Entrada{0, 1, 0, 1}))
 	fmt.Println(p)
 }
