a079c897dd79c21cc1d283be501c7cc224127519
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2013-06-06 02:33:31 -0400

    Inserção da AVL funcionando

--- Fucapi/2013_1/Analise/Trabalho 2/avl.py
+++ Fucapi/2013_1/Analise/Trabalho 2/avl.py
@@ -1,59 +1,93 @@
+class NodoNulo:
+	def __init__(self):
+		self.altura = 0
+
+Nulo = NodoNulo()
+
 class Nodo:
 	def __init__(self, num):
 		self.valor = num
-		self.altura = 0
-		self.esquerda = None
-		self.direita = None
+		self.altura = 1
+		self.esquerda = Nulo
+		self.direita = Nulo
 	
 	def fator(self):
-		return 2
+		return self.direita.altura - self.esquerda.altura
 	
 	def balanceia(self):
-		pass
+		fator = self.fator()
+		if abs(fator) > 1:
+			if fator > 0:
+				if self.direita.fator() < 0:
+					self.direita.rodaDir()
+				self.rodaEsq()
+			else:
+				if self.esquerda.fator() > 0:
+					self.esquerda.rodaEsq()
+				self.rodaDir()
+			
 	
 	def rodaEsq(self):
-		pass
+		filho = self.direita
+		
+		swap = filho.valor
+		filho.valor = self.valor
+		self.valor = swap
+		
+		self.direita = filho.direita
+		filho.direita = filho.esquerda
+		filho.esquerda = self.esquerda
+		self.esquerda = filho
 	
 	def rodaDir(self):
-		pass
+		filho = self.esquerda
+		
+		swap = filho.valor
+		filho.valor = self.valor
+		self.valor = swap
+		
+		self.esquerda = filho.esquerda
+		filho.esquerda = filho.direita
+		filho.direita = self.direita
+		self.direita = filho
 
 class AVL:
 	def __init__(self):
-		self.raiz = None
+		self.raiz = Nulo
 	
 	def insere(self, num):
-		if self.raiz is None:
+		if self.raiz == Nulo:
 			self.raiz = Nodo(num)
 		else:
 			atual = self.raiz
-			caminho = [atual]
+			caminho = []
 			
 			while True:
+				caminho.append(atual)
 				if num > atual.valor:
-					if atual.direita is None:
+					if atual.direita == Nulo:
 						atual.direita = Nodo(num)
 						break
 					else:
 						atual = atual.direita
-						caminho.append(atual)
 				else:
-					if atual.esquerda is None:
+					if atual.esquerda == Nulo:
 						atual.esquerda = Nodo(num)
 						break
 					else:
 						atual = atual.esquerda
-						caminho.append(atual)
 
 			while len(caminho) > 0:
-				atual = caminho.pop
-				if abs(atual.fator()) > 1:
-					atual.balanceia()
+				atual = caminho.pop()
+				
+				atual.altura = max(atual.esquerda.altura, atual.direita.altura) + 1
+				atual.balanceia()
 	
 	def imprime(self):
 		nos = []
 		atual = self.raiz
-		while len(nos) != 0 or atual is not None:
-			if atual is not None:
+		while len(nos) != 0 or atual != Nulo:
+			if atual != Nulo:
 				nos.append(atual)
 				atual = atual.esquerda
 			else:
