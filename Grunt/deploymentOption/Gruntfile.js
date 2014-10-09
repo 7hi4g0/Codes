c1f5875bc24b5831a067bdc186184b173ba083be
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2014-10-08 20:44:08 -0400

    Learning Grunt file capabilities

--- Grunt/deploymentOption/Gruntfile.js
+++ Grunt/deploymentOption/Gruntfile.js
@@ -2,15 +2,29 @@
 
 module.exports = function (grunt) {
 	grunt.config.init({
+		pkg: grunt.file.readJSON('package.json'),
 		copyFiles: {
 			options: {
 				workingDirectory: 'working',
 				manifest: [
-					'index.html', 'stylesheets/style.css', 'javascripts/app.js'
+					'index.html', 'stylesheets/', 'javascripts/'
 				]
 			}
 		}
 	});
+
+	var recursiveCopy = function (source, destination) {
+		if (grunt.file.isDir(source)) {
+
+			grunt.file.recurse(source, function (file) {
+				recursiveCopy(file, destination);
+			});
+
+		} else {
+			grunt.log.writeln('copying ' + source + ' to ' + destination);
+			grunt.file.copy(source, destination + '/' + source);
+		}
+	};
 
 	grunt.registerTask('createFolder',
 			'Create the working folder.', function () {
@@ -30,17 +44,19 @@ module.exports = function (grunt) {
 			'Copy files to working directory.', function () {
 		var files, workingDirectory;
 
-		grunt.config.requires('copyFiles.options.workingDirectory');
-		grunt.config.requires('copyFiles.options.manifest');
+		this.requiresConfig(this.name + '.options.manifest');
+		this.requiresConfig(this.name + '.options.workingDirectory');
 
-		files = grunt.config.get('copyFiles.options.manifest');
-		workingDirectory = grunt.config.get('copyFiles.options.workingDirectory');
+		files = this.options().manifest;
+		workingDirectory = this.options().workingDirectory;
 
 		files.forEach(function (file) {
-			var destination = workingDirectory + '/' + file;
-			grunt.log.writeln('copying ' + file + ' to ' + destination);
-			grunt.file.copy(file, destination);
+			recursiveCopy(file, workingDirectory);
 		});
+
+		var content = '<%= pkg.name %> version <%= pkg.version %>';
+		content = grunt.template.process(content);
+		grunt.file.write(workingDirectory + '/version.txt', content);
 	});
 
 	grunt.registerTask('deploy',
--- Grunt/deploymentRecursive/Gruntfile.js
+++ Grunt/deploymentRecursive/Gruntfile.js
@@ -2,15 +2,29 @@
 
 module.exports = function (grunt) {
 	grunt.config.init({
+		pkg: grunt.file.readJSON('package.json'),
 		copyFiles: {
 			options: {
 				workingDirectory: 'working',
 				manifest: [
-					'index.html', 'stylesheets/style.css', 'javascripts/app.js'
+					'index.html', 'stylesheets/', 'javascripts/'
 				]
 			}
 		}
 	});
+
+	var recursiveCopy = function (source, destination) {
+		if (grunt.file.isDir(source)) {
+
+			grunt.file.recurse(source, function (file) {
+				recursiveCopy(file, destination);
+			});
+
+		} else {
+			grunt.log.writeln('copying ' + source + ' to ' + destination);
+			grunt.file.copy(source, destination + '/' + source);
+		}
+	};
 
 	grunt.registerTask('createFolder',
 			'Create the working folder.', function () {
@@ -37,10 +51,12 @@ module.exports = function (grunt) {
 		workingDirectory = grunt.config.get('copyFiles.options.workingDirectory');
 
 		files.forEach(function (file) {
-			var destination = workingDirectory + '/' + file;
-			grunt.log.writeln('copying ' + file + ' to ' + destination);
-			grunt.file.copy(file, destination);
+			recursiveCopy(file, workingDirectory);
 		});
+
+		var content = '<%= pkg.name %> version <%= pkg.version %>';
+		content = grunt.template.process(content);
+		grunt.file.write(workingDirectory + '/version.txt', content);
 	});
 
 	grunt.registerTask('deploy',
