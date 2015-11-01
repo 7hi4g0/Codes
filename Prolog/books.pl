ae4738bd7c2d7045bedb4b8461238e29e296e55e
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2015-10-31 21:08:39 -0400

    Prolog's Day 1 of Seven Languages in Seven Weeks

---
+++ Prolog/books.pl
@@ -0,0 +1,26 @@
+book(fermats_last_theorem).
+book(the_code_book).
+book(the_simpsons_and_their_mathematical_secrets).
+book(the_universe_in_a_nutshell).
+book(on_the_shoulders_of_giants).
+book(the_grand_design).
+book(the_drunkards_walk).
+book(men_who_hate_women).
+book(the_girl_who_played_with_fire).
+book(the_air_castle_that_was_blown_up).
+book(good_math).
+book(the_man_who_counted).
+
+author(fermats_last_theorem, simon_singh).
+author(the_code_book, simon_singh).
+author(the_simpsons_and_their_mathematical_secrets, simon_singh).
+author(the_universe_in_a_nutshell, stephen_hawking).
+author(on_the_shoulders_of_giants, stephen_hawking).
+author(the_grand_design, stephen_hawking).
+author(the_grand_design, leonard_mlodinow).
+author(the_drunkards_walk, leonard_mlodinow).
+author(men_who_hate_women, stieg_larsson).
+author(the_girl_who_played_with_fire, stieg_larsson).
+author(the_air_castle_that_was_blown_up, stieg_larsson).
+author(good_math, mar_c_chu_carroll).
+author(the_man_who_counted, malba_tahan).
