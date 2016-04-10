19301c15eb980d1217e5569ba7341c7bcd580605
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2016-04-09 20:56:30 -0400

    Added blank lines to int types output

--- Linux/int_x64_types.c
+++ Linux/int_x64_types.c
@@ -8,21 +8,25 @@ int main() {
 	printf("__s32:                     \t%lu bytes\n", sizeof(__s32));
 	printf("__s64:                     \t%lu bytes\n", sizeof(__s64));
 
+	printf("\n");
 	printf("__u8:                      \t%lu bytes\n", sizeof(__u8));
 	printf("__16:                      \t%lu bytes\n", sizeof(__u16));
 	printf("__u32:                     \t%lu bytes\n", sizeof(__u32));
 	printf("__u64:                     \t%lu bytes\n", sizeof(__u64));
 
+	printf("\n");
 	printf("int8_t:                    \t%lu bytes\n", sizeof(int8_t));
 	printf("int16_t:                   \t%lu bytes\n", sizeof(int16_t));
 	printf("int32_t:                   \t%lu bytes\n", sizeof(int32_t));
 	printf("int64_t:                   \t%lu bytes\n", sizeof(int64_t));
 
+	printf("\n");
 	printf("uint8_t:                   \t%lu bytes\n", sizeof(uint8_t));
 	printf("uint16_t:                  \t%lu bytes\n", sizeof(uint16_t));
 	printf("uint32_t:                  \t%lu bytes\n", sizeof(uint32_t));
 	printf("uint64_t:                  \t%lu bytes\n", sizeof(uint64_t));
 
+	printf("\n");
 	printf("char:                      \t%lu bytes\n", sizeof(char));
 	printf("short int:                 \t%lu bytes\n", sizeof(short int));
 	printf("short:                     \t%lu bytes\n", sizeof(short));
@@ -32,6 +36,7 @@ int main() {
 	printf("long long int:             \t%lu bytes\n", sizeof(long long int));
 	printf("long long:                 \t%lu bytes\n", sizeof(long long));
 
+	printf("\n");
 	printf("unsigned char:             \t%lu bytes\n", sizeof(unsigned char));
 	printf("unsigned short int:        \t%lu bytes\n", sizeof(unsigned short int));
 	printf("unsigned short:            \t%lu bytes\n", sizeof(unsigned short));
